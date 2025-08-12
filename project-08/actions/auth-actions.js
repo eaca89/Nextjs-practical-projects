505c574411c62766a968b083d3b2b2e46e76938b
Ehsan <eaca89@gmail.com>
2025-08-12 15:33:00 -0400

    auth update

--- project-08/actions/auth-actions.js
+++ project-08/actions/auth-actions.js
@@ -1,6 +1,9 @@
 "use server";
 
-import { createUser } from "@/lib/user";
+import { createAuthSession, destroySession } from "@/lib/auth";
+import { hashUserPassword, verifyPassword } from "@/lib/hash";
+import { createUser, getUserByEmail } from "@/lib/user";
+import { redirect } from "next/navigation";
 
 export async function signup(prevState, formData) {
   const email = formData.get("email");
@@ -8,7 +11,7 @@ export async function signup(prevState, formData) {
 
   let errors = {};
 
-  if (email.includes("@")) {
+  if (!email.includes("@")) {
     errors.email = "Please enter a valid email address.";
   }
 
@@ -22,5 +25,62 @@ export async function signup(prevState, formData) {
     };
   }
 
-  createUser(email, password);
+  const hashedPassword = hashUserPassword(password);
+  try {
+    const id = createUser(email, hashedPassword);
+    await createAuthSession(id);
+    redirect("/training");
+  } catch (error) {
+    if (error.code === "SQLITE_CONSTRAINT_UNIQUE") {
+      return {
+        errors: {
+          email:
+            "It seems like an email account for the chosed email already exists.",
+        },
+      };
+    }
+    throw error;
+  }
+}
+
+export async function login(prevState, formData) {
+  const email = formData.get("email");
+  const password = formData.get("password");
+
+  const existingUser = getUserByEmail(email);
+
+  if (!existingUser) {
+    return {
+      errors: {
+        email: "Could not authenticte user, please check your credentials.",
+      },
+    };
+  }
+
+  const isValidPassword = verifyPassword(existingUser.password, password);
+
+  if (!isValidPassword) {
+    return {
+      errors: {
+        password: "Could not authenticte user, please check your credentials.",
+      },
+    };
+  }
+
+  await createAuthSession(existingUser.id);
+  redirect("/training");
+}
+
+// a helper server action
+export async function auth(mode, prevState, formData) {
+  if (mode === "login") {
+    return login(prevState, formData);
+  }
+
+  return signup(prevState, formData);
+}
+
+export async function logout() {
+  await destroySession();
+  redirect("/");
 }
